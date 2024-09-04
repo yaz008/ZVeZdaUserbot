7f1e2151b165966744ca8394f44d6e95a27b7cb3
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-09-04 12:59:24 +0300

    Add edit_message

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
+from os import getenv
 from app import app
 from pyrogram.client import Client
+from pyrogram.filters import user
 from pyrogram.types import Message
 
-@app.on_message(filters=None)
+def edit_message(text: str) -> str:
+    table: dict[str, str] = { 'з': 'Z', 'в': 'V', 'З': 'Z', 'В': 'V'}
+    return text.translate(text.maketrans(table))
+
+@app.on_message(filters=user(getenv(key='ME')))
 def on_message(_: Client, message: Message) -> None:
-    print(message.text)
+    message.edit_text(text=edit_message(message.text))
 
 if __name__ == '__main__':
     app.run()
