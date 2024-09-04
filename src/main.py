d37449b3da2ac1001664cdbf37495c7be337fc97
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-09-04 13:22:17 +0300

    Add SVO to edit_message

--- src/main.py
+++ src/main.py
@@ -5,12 +5,15 @@ from pyrogram.filters import user
 from pyrogram.types import Message
 
 def edit_message(text: str) -> str:
-    table: dict[str, str] = { 'з': 'Z', 'в': 'V', 'З': 'Z', 'В': 'V'}
+    table: dict[str, str] = { 'з': 'Z', 'в': 'V', 'З': 'Z', 'В': 'V',
+                             'сво': 'СВО', 'свО': 'СВО', 'сВо': 'СВО', 'сВО': 'СВО',
+                             'Сво': 'СВО', 'СвО': 'СВО', 'СВо': 'СВО'}
     return text.translate(text.maketrans(table))
 
 @app.on_message(filters=user(getenv(key='ME')))
 def on_message(_: Client, message: Message) -> None:
-    message.edit_text(text=edit_message(message.text))
+    if any([x in message.text.lower() for x in ('з', 'в', 'сво')]):
+        message.edit_text(text=edit_message(message.text))
 
 if __name__ == '__main__':
     app.run()
